b412e6cdf8f872ddd620dbc0e3ad80566a62e6c9
Vinícius Aiala <vinicius.aiala@hotmail.com>
2021-06-04 14:20:39 -0300

    Correção das funções de multiplicação

--- funcMult.py
+++ funcMult.py
@@ -1,13 +1,16 @@
 def multMatriz(m1, m2):
-  matrizResult = [[0] * 4, [0] * 4, [0] * 4, [0] * 4]
-  for i in range (4):
-      for j in range (4):
-          soma = 0
-          for k in range (4):
-              soma += m1[j][k] * m2[k][i]
-          matrizResult[j][i] = soma
-    
-  return (matrizResult);    
+    matrizResult = [[0] * 4, [0] * 4, [0] * 4, [0] * 4]
+
+    for i in range(4):
+        for j in range(4):
+            soma = 0
+            for k in range(4):
+
+                soma += m1[j][k] * m2[k][i]
+            matrizResult[j][i] = soma
+
+    return matrizResult
+
 
 def mult_vetor_matriz(v, m):
     vetor_result = [0] * 4
