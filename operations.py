1084e57cf711398290a83693d4fd70acd313f494
Vinícius Aiala <vinicius.aiala@hotmail.com>
2021-06-04 19:30:47 -0300

    Pequenas correções

--- operations.py
+++ operations.py
@@ -28,18 +28,21 @@ def mult_vetor_matriz(v, m):
     return vetor_result[0:3]
 
 def product_vetorial(v1, v2):
+    result = [0] * 3
     result[0] = (v1[1]*v2[2] - v1[2]*v2[1]);
     result[1] = -(v1[0]*v2[2] - v1[2]*v2[0]);
     result[2] = (v1[0]*v2[1] - v1[1]*v2[0]);
     return result;
 
 def division_vetorial(v1, v2):
+    result = [0] * 3
     result[0] = v1[0]/v2[0];
     result[1] = v1[1]/v2[1];
     result[2] = v1[2]/v2[2];
     return result;
 
 def minus_vetorial(v1, v2):
+    result = [0] * 3
     result[0] = v1[0] - v2[0];
     result[1] = v1[1] - v2[1];
     result[2] = v1[2] - v2[2];
@@ -48,7 +51,7 @@ def minus_vetorial(v1, v2):
 def modulo_vetorial(v):
     v[0] = abs(v[0]);
     v[1] = abs(v[1]);
-    v[2] = abs(v[]);
+    v[2] = abs(v[2]);
     return v;
     
 
